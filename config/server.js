dc48218499164362a952f9fe6709ba09b3854bd9
root <root@DESKTOP-SKK4J02.localdomain>
2021-07-03 01:08:00 +0300

    initial commit for dj-backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6a2bad1f4365e3b47ee7c12a742e63b0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '70b94384-20e4-46c0-a43c-f2ebee43f2cd'
+};
