fdb879b44df380bf362419cd52296790bcd161c5
root <root@DESKTOP-SKK4J02.localdomain>
2021-07-06 00:14:59 +0300

    finished from auth(login,logout,register), backend almost done :)

--- config/server.js
+++ config/server.js
@@ -3,8 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '6a2bad1f4365e3b47ee7c12a742e63b0'),
-      email: "mostafafayyado1@gmail.com"
+      secret: env('ADMIN_JWT_SECRET', '6a2bad1f4365e3b47ee7c12a742e63b0')
     },
   },
 });
